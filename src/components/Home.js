8036c4770d23c011a1e1c65578af92193c12e79d
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-03-11 18:54:34 -0300

    Add popcorn image, text and edit css home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass, faHouseChimney } from "@fortawesome/free-solid-svg-icons"
+import moviePop from "../assets/moviePopIcon.png"
 
 export default function Home() {
     return (
-        <div>
-            <FontAwesomeIcon style={{ color: "white", marginRight: "15px", fontSize: "33px" }} icon={faHouseChimney} />
-            <FontAwesomeIcon style={{ color: "white", fontSize: "33px" }} icon={faMagnifyingGlass} />
+        <div className='home-container'>
+            <div className='home-icons-container'>
+                <FontAwesomeIcon className='home-icon' icon={faHouseChimney} />
+                <FontAwesomeIcon className='search-icon' icon={faMagnifyingGlass} />
+            </div>
+            <div style={{ textAlign: "center" }}>
+                <img src={moviePop} alt="movieLogo" />
+            </div>
+            <div>
+                <p className='text-moviepop'>MoviePop!</p>
+            </div>
         </div>
     )
 }
